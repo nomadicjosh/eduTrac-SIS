801bd4af9069044d52a43c42fa88cc78c44e9c3a
Joshua Parker <joshmac3@icloud.com>
2015-10-08 09:10:47 -0400

    Bumped up release value.

--- app/core-function.php
+++ app/core-function.php
@@ -11,7 +11,7 @@ if (!defined('BASE_PATH'))
  * @author      Joshua Parker <joshmac3@icloud.com>
  */
 define('CURRENT_RELEASE', '6.0.00');
-define('RELEASE_TAG', '6.1.05');
+define('RELEASE_TAG', '6.1.06');
 
 $app = \Liten\Liten::getInstance();
 
