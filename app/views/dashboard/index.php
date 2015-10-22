f7b060abcc6cecb35a75ff5bbb85314a97284fc0
Joshua Parker <joshmac3@icloud.com>
2015-10-22 10:03:08 -0400

    Updated file header.

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -2,16 +2,13 @@
 /**
  * Dashboard View
  *  
- * PHP 5.4+
- *
- * eduTrac(tm) : Student Information System (http://www.7mediaws.org/)
- * @copyright (c) 2013 7 Media Web Solutions, LLC
+ * @license GPLv3
  * 
- * @link        http://www.7mediaws.org/
  * @since       3.0.0
- * @package     eduTrac
- * @author      Joshua Parker <josh@7mediaws.org>
+ * @package     eduTrac SIS
+ * @author      Joshua Parker <joshmac3@icloud.com>
  */
+
 $app = \Liten\Liten::getInstance();
 $app->view->extend('_layouts/dashboard');
 $app->view->block('dashboard');
