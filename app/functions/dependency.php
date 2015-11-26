4ed1f58ea56ce5801b7d5352d9e09d2e2ea55cb2
Joshua Parker <joshmac3@icloud.com>
2015-11-26 16:57:40 -0500

    Added autoloader for new core updater library.

--- app/functions/dependency.php
+++ app/functions/dependency.php
@@ -12,7 +12,8 @@ if (!defined('BASE_PATH'))
  */
 $app = \Liten\Liten::getInstance();
 
-include_once(APP_PATH . 'src/Httpful/autoload.php');
+require_once(APP_PATH . 'src/Httpful/autoload.php');
+require_once(APP_PATH . 'src/CoreUpdate/vendor/autoload.php');
 
 $app->inst->singleton('hook', function () {
     return new \app\src\Hooks();
