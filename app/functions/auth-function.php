fae4ac0c543096491c23db4b4bee1013ae23cb8d
Joshua Parker <joshmac3@icloud.com>
2016-01-11 22:15:14 -0500

    Updated get_person_by function.

--- app/functions/auth-function.php
+++ app/functions/auth-function.php
@@ -391,7 +391,8 @@ function get_person_by($field, $value)
     $app = \Liten\Liten::getInstance();
 
     $person = $app->db->person()
-    ->select('person.*, address.*, staff.*, student.*')
+    ->select('person.personID, person.altID, person.uname, person.email')
+    ->select('address.*, staff.*, student.*')
     ->_join('address', 'person.personID = address.personID')
     ->_join('staff', 'person.personID = staff.staffID')
     ->_join('student', 'person.personID = student.stuID')
