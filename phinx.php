cfd6c48d9092c15ac008224db35847f51df87f16
Joshua Parker <joshmac3@icloud.com>
2016-09-21 15:49:58 -0400

    Updated phinx config file.

--- phinx.php
+++ phinx.php
@@ -11,11 +11,16 @@
 
 date_default_timezone_set('UTC');
 
-defined('DB_HOST') or define('DB_HOST', '');
-defined('DB_NAME') or define('DB_NAME', '');
-defined('DB_USER') or define('DB_USER', '');
-defined('DB_PASS') or define('DB_PASS', '');
-defined('DB_PORT') or define('DB_PORT', 3306);
+/**
+ * If you are installing on a development server such
+ * as WAMP, MAMP, XAMPP or AMPPS, you might need to
+ * set DB_HOST to 127.0.0.1 instead of localhost.
+ */
+defined('DB_HOST') or define('DB_HOST', ''); // MySQL server host.
+defined('DB_NAME') or define('DB_NAME', ''); // Database name.
+defined('DB_USER') or define('DB_USER', ''); // Database username.
+defined('DB_PASS') or define('DB_PASS', ''); // Database password.
+defined('DB_PORT') or define('DB_PORT', 3306); // Database port.
     
 return array(
     "paths" => array(
