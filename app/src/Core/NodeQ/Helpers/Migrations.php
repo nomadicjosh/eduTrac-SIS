fabc955d09fbd90f2af231288720da00872ba9d8
Joshua Parker <joshmac3@icloud.com>
2016-10-10 15:32:51 -0400

    Implement feature #63 @3h.

---
+++ app/src/Core/NodeQ/Helpers/Migrations.php
@@ -0,0 +1,78 @@
+<?php namespace app\src\Core\NodeQ\Helpers;
+
+use \app\src\Core\NodeQ\etsis_NodeQ as Node;
+
+/**
+ * Data managing class
+ * 
+ * @since 6.2.11
+ */
+class Migrations
+{
+
+    public static function dispense($table)
+    {
+        if (!Validate::table($table)->exists()) {
+            return self::$table();
+        }
+        return true;
+    }
+
+    public function reset_password()
+    {
+        Node::create('reset_password', [
+            'personid' => 'integer',
+            'uname' => 'string',
+            'email' => 'string',
+            'name' => 'string',
+            'fname' => 'string',
+            'lname' => 'string',
+            'password' => 'string',
+            'sent' => 'integer'
+        ]);
+    }
+
+    public function csv_email()
+    {
+        Node::create('csv_email', [
+            'recipient' => 'string',
+            'message' => 'string',
+            'subject' => 'string',
+            'filename' => 'string',
+            'sent' => 'integer'
+        ]);
+    }
+
+    public function login_details()
+    {
+        Node::create('login_details', [
+            'personid' => 'integer',
+            'uname' => 'string',
+            'email' => 'string',
+            'fname' => 'string',
+            'lname' => 'string',
+            'password' => 'string',
+            'altid' => 'string',
+            'sent' => 'integer'
+        ]);
+    }
+    
+    public function change_address()
+    {
+        Node::create('change_address', [
+            'personid' => 'integer',
+            'uname' => 'string',
+            'fname' => 'string',
+            'lname' => 'string',
+            'email' => 'string',
+            'address1' => 'string',
+            'address2' => 'string',
+            'city' => 'string',
+            'state' => 'string',
+            'zip' => 'string',
+            'country' => 'string',
+            'phone' => 'string',
+            'sent' => 'integer'
+        ]);
+    }
+}
---
+++ app/src/Core/NodeQ/LazerException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\src\Core\NodeQ;
+
+/**
+ * Exception extend
+ *
+ * @category Exceptions
+ * @author Grzegorz Kuźnik
+ * @copyright (c) 2013, Grzegorz Kuźnik
+ */
+class LazerException extends \Exception {
+    
+}
