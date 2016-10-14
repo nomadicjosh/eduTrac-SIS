8a88243b25c3368169f3d436ee01ac80c6961f5d
Joshua Parker <joshmac3@icloud.com>
2016-10-14 15:38:47 -0400

    Added new libraries to vendor.

--- app/src/vendor/composer/autoload_psr4.php
+++ app/src/vendor/composer/autoload_psr4.php
@@ -23,5 +23,7 @@ return array(
     'Joli\\JoliNotif\\' => array($vendorDir . '/jolicode/jolinotif/src'),
     'Gettext\\Languages\\' => array($vendorDir . '/gettext/languages/src'),
     'Gettext\\' => array($vendorDir . '/gettext/gettext/src'),
+    'Desarrolla2\\Test\\Cache\\' => array($vendorDir . '/desarrolla2/cache/test'),
+    'Desarrolla2\\Cache\\' => array($vendorDir . '/desarrolla2/cache/src'),
     'Cascade\\' => array($vendorDir . '/theorchard/monolog-cascade/src'),
 );
