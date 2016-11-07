cccb02913ddeaa1d4c1408af5ff82c5e92de7736
Joshua Parker <joshmac3@icloud.com>
2016-11-07 09:08:20 -0500

    Added jQuery QueryBuilder library for rules engine.

---
+++ static/assets/js/script.js
@@ -0,0 +1,39 @@
+/**
+ * Projet Name : Dynamic Form Processing with PHP
+ * URL: http://techstream.org/Web-Development/PHP/Dynamic-Form-Processing-with-PHP
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Tech Stream
+ * http://techstream.org
+ */
+
+function addRow(tableID) {
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    var colCount = table.rows[0].cells.length;
+    for (var i = 0; i < colCount; i++) {
+        var newcell = row.insertCell(i);
+        newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+    }
+}
+
+function deleteRow(tableID) {
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+    for (var i = 0; i < rowCount; i++) {
+        var row = table.rows[i];
+        var chkbox = row.cells[0].childNodes[0];
+        if (null != chkbox && true == chkbox.checked) {
+            if (rowCount <= 1) { 						// limit the user from removing all the fields
+                alert("Cannot Remove all rows");
+                break;
+            }
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+        }
+    }
+}
