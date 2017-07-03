bdcd8164475856e05497b3b325d85154b5926c51
Joshua Parker <joshmac3@icloud.com>
2017-07-03 10:11:52 -0400

    Fix error with courseID and added prereq rule.

--- app/functions/course-function.php
+++ app/functions/course-function.php
@@ -4,6 +4,7 @@ if (!defined('BASE_PATH'))
 use app\src\Core\Exception\NotFoundException;
 use app\src\Core\Exception\Exception;
 use PDOException as ORMException;
+use Cascade\Cascade;
 
 /**
  * eduTrac SIS Course Functions
@@ -28,7 +29,7 @@ function courseList($id = '')
             ->where('courseID <> ?', $id)->_and_()
             ->where('currStatus = "A"')->_and_()
             ->where('endDate IS NULL')->_or_()
-            ->whereLte('endDate','0000-00-00');
+            ->whereLte('endDate', '0000-00-00');
         $q = $crse->find(function ($data) {
             $array = [];
             foreach ($data as $d) {
@@ -68,10 +69,10 @@ function get_course($course, $object = true)
     if ($course instanceof \app\src\Core\etsis_Course) {
         $_course = $course;
     } elseif (is_array($course)) {
-        if (empty($course['id'])) {
+        if (empty($course['courseID'])) {
             $_course = new \app\src\Core\etsis_Course($course);
         } else {
-            $_course = \app\src\Core\etsis_Course::get_instance($course['id']);
+            $_course = \app\src\Core\etsis_Course::get_instance($course['courseID']);
         }
     } else {
         $_course = \app\src\Core\etsis_Course::get_instance($course);
@@ -86,4 +87,46 @@ function get_course($course, $object = true)
     }
 
     return $_course;
+}
+
+/**
+ * Checks to see if student meets the prerequisite rule requirements.
+ * 
+ * @since 6.3.0
+ * @param int $stuID Unique student ID.
+ * @param int $crseID Unique course ID.
+ * @return boolean
+ */
+function etsis_prereq_rule($stuID, $crseID)
+{
+    $app = \Liten\Liten::getInstance();
+
+    $crse = get_course($crseID);
+    try {
+        $rule = _escape($crse->rule);
+        if ($rule != null) {
+            $prrl = $app->db->query(
+                    "SELECT v_sacp.stuID FROM v_sacp"
+                    . " INNER JOIN stal ON v_sacp.stuID = stal.stuID AND v_sacp.prog = stal.acadProgCode"
+                    . " INNER JOIN v_scrd ON v_sacp.stuID = v_scrd.stuID AND v_sacp.prog = v_scrd.prog"
+                    . " WHERE v_sacp.stuID = ?"
+                    . " AND $rule", [$stuID]
+                )
+                ->findOne();
+
+            if (_h($prrl->stuID) <> $stuID) {
+                return false;
+            }
+        }
+        return true;
+    } catch (NotFoundException $e) {
+        Cascade::getLogger('error')->error($e->getMessage());
+        _etsis_flash()->error(_etsis_flash()->notice(409));
+    } catch (ORMException $e) {
+        Cascade::getLogger('error')->error($e->getMessage());
+        _etsis_flash()->error(_etsis_flash()->notice(409));
+    } catch (Exception $e) {
+        Cascade::getLogger('error')->error($e->getMessage());
+        _etsis_flash()->error(_etsis_flash()->notice(409));
+    }
 }
